8e55b103810debb33366ae946b432cdf2913b6f1
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-11-15 08:46:21 +1100

    feat(capitalize): Complete tests for capitalize fn
    Tests will now test if the first letter is indeed
    a letter, if the parameter provided to the
    function is a string, if the string is one word
    and not a sentence

--- capitalize.js
+++ capitalize.js
@@ -1,7 +1,25 @@
 export default function capitalize(string) {
-  if (string) {
-    return string;
+  let splitString;
+  let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let validCharacters = alphabet.split("");
+
+  if (!string) {
+    throw new Error("Error: function requires a value");
+  } else if (typeof string !== "string") {
+    throw new Error("Error: function requires value to be a string");
   } else {
-    throw new Error();
+    splitString = string.split("");
+    if (!validCharacters.includes(splitString[0])) {
+      throw new Error("Error: First letter is not a letter");
+    }
+    for (let letter of splitString) {
+      if (letter === " ") {
+        throw new Error(
+          "Error: Cannot input sentences, input must be a single word"
+        );
+      }
+    }
+    splitString[0] = splitString[0].toUpperCase();
+    return splitString.join("");
   }
 }
--- capitalize.test.js
+++ capitalize.test.js
@@ -7,3 +7,26 @@ test("function returns something", () => {
 test("function returns error with no input", () => {
   expect(() => capitalize()).toThrow(Error);
 });
+
+test("error message for no input explains issue", () => {
+  expect(() => capitalize()).toThrow("Error: function requires a value");
+});
+
+test("input is a string", () => {
+  expect(() => capitalize(123)).toThrow(
+    "Error: function requires value to be a string"
+  );
+});
+
+test("first letter is a letter", () => {
+  expect(() => capitalize("1donkey")).toThrow(Error);
+  expect(() => capitalize("2donkey")).toThrow(Error);
+});
+
+test("first letter becomes uppercase", () => {
+  expect(capitalize("donkey")).toBe("Donkey");
+});
+
+test("cannot input a sentence", () => {
+  expect(() => capitalize("mango iguana pirahna")).toThrow(Error);
+});
